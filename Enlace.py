482ddb43bd895cc8ef7364f4b4a7e5c6318476b1
MBezerril <moisesc.bezerril@gmail.com>
2018-11-03 14:45:11 -0300

    Correção do enlace

--- Enlace.py
+++ Enlace.py
@@ -1,14 +1,15 @@
 import Fisica as fis
 import threading as thrd
 from time import sleep
-import Ramming as hamm
+import Hamming as hamm
+import random
 class CamadaEnlace:
     def __init__(self):
         self.camadaFisica = fis.CamadaFisica() # Camada física
         self.colision = False # variavel pra detectar colisão
         self.tries = 10 # Quantidade de tentativas
         self.transmiting = False # Caso a camada de enlace esteja fazendo tranmissão
-        self.bits = 10
+        self.receiving = False #caso esteja recebendo dados
     def trasnmition(self, data):
         """
         Quando receber um dado tenta transmistir os dados pela camada física
@@ -17,28 +18,42 @@ class CamadaEnlace:
         :param data:
         :return:
         """
-
-
-
+        data = hamm.hammingCodes(data)
+        data.append('borda')
+        data.insert(0, 'borda')
         for t in range(self.tries): # Verifica a disponibilidade da rede uma certa quantidade de vezes antes de expirar
-            lido = self.camadaFisica.read(self.bits)
-            if 1 not in array:
+            lido = self.camadaFisica.read()
+            if lido is None:
                 self.transmiting = True
                 break
+            else:
+                sleep(random.randint(10, 20)) #Espera aleatória para verificar novamente se o meio está livre
+        # Se foi dado o sinal de que pode ser transmitido, ele escreve e le para verificar colisao
         if self.transmiting:
-            writer = thrd.Thread(target=self.camadaFisica.write, args=(data,))
-            writer.start()
+            for bit in data:
+                self.camadaFisica.write(bit)
+                if self.camadaFisica.read() != bit: #Se foi lido algo diferente do que foi escrito, então houve colisão
+                    print("COLISÃO")
+            return True
             # self.camadaFisica.write(data)
         else:
             print("EXCEDEU AS TENTATIVAS - CAMADA DE ENLACE")
-        #sleep(2)
-        #self.camadaFisica.stopWrite()
-        #while writer.is_alive():
-        #   pass
-        # Deteccao de colisão aqui <<<<<<<<<<<<<<<<<<<<<<<<
-        # Para para a escrita, basta chamar self.camadaFisica.stopWrite()
-
-teste1 = CamadaEnlace()
-
-teste1.trasnmition([0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1])
-teste2 = fis.CamadaFisica()
+            return False
+    def listenPackage(self):
+        recebido = 0
+        quadro = []
+        #self.executor = thrd.Thread(target=self._writing, args=[data])
+        while not self.transmiting:
+            recebido = self.camadaFisica.read()
+            if recebido == 'borda':
+                self.receiving = True
+                break
+        while not self.transmiting and self.receiving:
+            recebido = self.camadaFisica.read()
+            if recebido != 'borda':
+                quadro.append(recebido)
+            else:
+                self.receiving = False
+        quadro = hamm.hammingCorrection(quadro)
+        #Processar o quadro para retornar o pacote
+        return quadro
